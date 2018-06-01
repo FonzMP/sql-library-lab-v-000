57f0e28dcade45b7f9652b85dec614b3cae5a646
Alex Wilson <fonzmp@gmail.com>
2018-06-01 20:47:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO series (title, author_id, subgenre_id)
 VALUES ("Dragonbird", 3, 2),
-("Toxic Avenger", 1, 2)
+("Toxic Avenger", 1, 2);
+
+INSERT INTO sub-genres (name)
+VALUES ("Comedy"),
+("Drama");
