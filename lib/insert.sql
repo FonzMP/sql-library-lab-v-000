f3b5cf3fdf2fb27996e1c7175a7dd77ae11cd68f
Alex Wilson <fonzmp@gmail.com>
2018-06-01 21:09:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,10 +20,10 @@ VALUES ("Another Trip Round the Sun", 1986, 2),
 
 INSERT INTO characters (name, motto, species, author_id, series_id)
 VALUES ("Alex", "Permanent Vacation", "Human", 1, 1),
-("John", "Sure", "Human", 1, 2),
-("Jacob", "Whatever", "Human", 1, 2),
+("John", "Sure", "Human", 1, 1),
+("Jacob", "Whatever", "Human", 1, 1),
 ("Cale", "GO FASTER", "Human", 1, 1),
-("Jack", "That's Not Right", "Human", 1, 3),
-("Chris", "It's mine!", "Human", 1, 4),
-("Marc", "Tacos", "Human", 1, 3),
-("Darryl", "Left Turn", "Human", 1, 4);
+("Jack", "That's Not Right", "Human", 1, 2),
+("Chris", "It's mine!", "Human", 1, 2),
+("Marc", "Tacos", "Human", 1, 2),
+("Darryl", "Left Turn", "Human", 1, 2);
