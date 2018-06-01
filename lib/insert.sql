c9268f898b14c37486caa2c57d13f3238ac06386
Alex Wilson <fonzmp@gmail.com>
2018-06-01 21:02:14 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,26 +1,26 @@
 INSERT INTO series (title)
 VALUES ("Dragonbird"),
 ("Toxic Avenger");
-
-INSERT INTO subgenres (name)
-VALUES ("Comedy"),
-("Drama");
-
-INSERT INTO authors (name)
-VALUES ("Jon Bon Jovi"),
-("Darryl Waltrip");
-
-INSERT INTO books (title, year, series_id)
-VALUES ("Another Trip Round the Sun", 1986, 2),
-("Here Comes the Sun", 2016, 3),
-("Where's the Sun", 1976, 1),
-
-INSERT INTO characters (name, motto, species, author_id, series_id)
-VALUES ("Alex", "Permanent Vacation", "Human", 1, 1),
-("John", "Sure", "Human", 1, 2),
-("Jacob", "Whatever", "Human", 1, 2),
-("Cale", "GO FASTER", "Human", 1, 1),
-("Jack", "That's Not Right", "Human", 1, 3),
-("Chris", "It's mine!", "Human", 1, 4),
-("Marc", "Tacos", "Human", 1, 3),
-("Darryl", "Left Turn", "Human", 1, 4);
+-- 
+-- INSERT INTO subgenres (name)
+-- VALUES ("Comedy"),
+-- ("Drama");
+-- 
+-- INSERT INTO authors (name)
+-- VALUES ("Jon Bon Jovi"),
+-- ("Darryl Waltrip");
+-- 
+-- INSERT INTO books (title, year, series_id)
+-- VALUES ("Another Trip Round the Sun", 1986, 2),
+-- ("Here Comes the Sun", 2016, 3),
+-- ("Where's the Sun", 1976, 1),
+-- 
+-- INSERT INTO characters (name, motto, species, author_id, series_id)
+-- VALUES ("Alex", "Permanent Vacation", "Human", 1, 1),
+-- ("John", "Sure", "Human", 1, 2),
+-- ("Jacob", "Whatever", "Human", 1, 2),
+-- ("Cale", "GO FASTER", "Human", 1, 1),
+-- ("Jack", "That's Not Right", "Human", 1, 3),
+-- ("Chris", "It's mine!", "Human", 1, 4),
+-- ("Marc", "Tacos", "Human", 1, 3),
+-- ("Darryl", "Left Turn", "Human", 1, 4);
