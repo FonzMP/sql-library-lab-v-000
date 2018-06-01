9d097308f67a5e5ff4f51c9f128127728d669192
Alex Wilson <fonzmp@gmail.com>
2018-06-01 21:05:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -13,7 +13,7 @@ VALUES ("Jon Bon Jovi"),
 INSERT INTO books (title, year, series_id)
 VALUES ("Another Trip Round the Sun", 1986, 2),
 ("Here Comes the Sun", 2016, 3),
-("Where's the Sun", 1976, 1),
+("Where's the Sun", 1976, 1);
 
 -- INSERT INTO characters (name, motto, species, author_id, series_id)
 -- VALUES ("Alex", "Permanent Vacation", "Human", 1, 1),
