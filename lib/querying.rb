569f173d3db261a470ea053bd3e584d92e8e2d20
Alex Wilson <fonzmp@gmail.com>
2018-06-02 21:28:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,9 +14,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species FROM characters
-  GROUP BY characters.species
-  ORDER BY characters.species
-  LIMIT 1"
+  GROUP BY characters.species"
 end
 
 def select_name_and_series_subgenres_of_authors
