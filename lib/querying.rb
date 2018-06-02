5b6c3464c7f279ad5e2f642814cc4c06f2e106af
Alex Wilson <fonzmp@gmail.com>
2018-06-02 21:39:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-  JOIN books ON books.series_id = series.id"
+  JOIN books ON books.series_id = series.id
+  JOIN characters ON characters.series_id = books.series_id
+  GROUP BY characters.species
+  ORDER BY characters.species DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
