5140ec9256bec1fefcabc73b051d5350b0eb033c
Alex Wilson <fonzmp@gmail.com>
2018-06-02 20:15:29 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,4 +29,17 @@ VALUES ("Alex", "Permanent Vacation", "Human", 1, 1),
 ("Darryl", "Left Turn", "Human", 1, 2);
 
 INSERT INTO character_books (book_id, character_id)
-  VALUES ();
+VALUES (1, 1),
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 1),
+(4, 1),
+(5, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(7, 1),
+(8, 1),
+(7, 1),
+;
